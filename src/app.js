142fb248260304f8191a1c248bd5e23140f38e7a
alvinphang <alvin.phang@accenture.com>
2019-12-19 20:22:35 +0800

    Refactoring & Security 1. Converted callback style code to use `async/await` 2. instead of constructing sql string, use parameterized query

--- src/app.js
+++ src/app.js
@@ -5,6 +5,49 @@ const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
 module.exports = (db) => {
+  function paginatedResults(model, page = 1, limit = 10) {
+    const results = {};
+    page = Number(page);
+    limit = Number(limit);
+
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    if (endIndex < model.length) {
+      results.next = {
+        page: page + 1,
+        limit,
+      };
+    }
+
+    if (startIndex > 0) {
+      results.previous = {
+        page: page - 1,
+        limit,
+      };
+    }
+
+    results.results = model.slice(startIndex, endIndex);
+    return results;
+  }
+
+  function insertAsync(sql, values = []) {
+    return new Promise((resolve, reject) => {
+      db.run(sql, values, function (err) {
+        if (err) reject(err);
+        resolve(this.lastID);
+      });
+    });
+  }
+
+  function selectAllAsync(sql, values = []) {
+    return new Promise((resolve, reject) => {
+      db.all(sql, values, function (err, rows) {
+        if (err) reject(err);
+        resolve(rows);
+      });
+    });
+  }
   /**
     * @swagger
     * /health:
@@ -18,7 +61,6 @@ module.exports = (db) => {
     */
   app.get('/health', (req, res) => res.send('Healthy'));
 
-
   /**
     * @swagger
     * /rides:
@@ -29,7 +71,7 @@ module.exports = (db) => {
     *   parameters:
     *   -   in: body
     *       name: body
-    *       description: the ride to create
+    *       description: the ride object to be created
     *       schema:
     *           type: object
     *           properties:
@@ -73,7 +115,7 @@ module.exports = (db) => {
     *                   type: string
     *
     */
-  app.post('/rides', jsonParser, (req, res) => {
+  app.post('/rides', jsonParser, async (req, res) => {
     const startLatitude = Number(req.body.start_lat);
     const startLongitude = Number(req.body.start_long);
     const endLatitude = Number(req.body.end_lat);
@@ -107,14 +149,14 @@ module.exports = (db) => {
     if (typeof driverName !== 'string' || driverName.length < 1) {
       return res.send({
         error_code: 'VALIDATION_ERROR',
-        message: 'Rider name must be a non empty string',
+        message: 'Driver name must be a non empty string',
       });
     }
 
     if (typeof driverVehicle !== 'string' || driverVehicle.length < 1) {
       return res.send({
         error_code: 'VALIDATION_ERROR',
-        message: 'Rider name must be a non empty string',
+        message: 'Vehichle name must be a non empty string',
       });
     }
 
@@ -128,25 +170,20 @@ module.exports = (db) => {
       req.body.driver_vehicle,
     ];
 
-    db.run('INSERT INTO Rides(startLat, startLong, endLat, endLong, riderName, driverName, driverVehicle) VALUES (?, ?, ?, ?, ?, ?, ?)', values, function (err) {
+    try {
+      const sql = 'INSERT INTO Rides(startLat, startLong, endLat, endLong, riderName, driverName, driverVehicle) VALUES (?, ?, ?, ?, ?, ?, ?)';
+      const lastID = await insertAsync(sql, values);
+      const rows = await selectAllAsync('SELECT * FROM Rides WHERE rideID = ?', [lastID]);
+
+      res.send(rows);
+    } catch (err) {
       if (err) {
         return res.send({
           error_code: 'SERVER_ERROR',
           message: 'Unknown error',
         });
       }
-
-      db.all('SELECT * FROM Rides WHERE rideID = ?', this.lastID, function (err2, rows) {
-        if (err2) {
-          return res.send({
-            error_code: 'SERVER_ERROR',
-            message: 'Unknown error',
-          });
-        }
-
-        res.send(rows);
-      });
-    });
+    }
   });
 
   /**
@@ -197,14 +234,10 @@ module.exports = (db) => {
     *                       type: string
     *
     */
-  app.get('/rides', (req, res) => {
-    db.all('SELECT * FROM Rides', function (err, rows) {
-      if (err) {
-        return res.send({
-          error_code: 'SERVER_ERROR',
-          message: 'Unknown error',
-        });
-      }
+  app.get('/rides', async (req, res) => {
+    let rows = [];
+    try {
+      rows = await selectAllAsync('SELECT * FROM Rides');
 
       if (rows.length === 0) {
         return res.send({
@@ -213,9 +246,15 @@ module.exports = (db) => {
         });
       }
 
-
       res.send(paginatedResults(rows, req.query.page, req.query.limit));
-    });
+    } catch (err) {
+      if (err) {
+        return res.send({
+          error_code: 'SERVER_ERROR',
+          message: 'Unknown error',
+        });
+      }
+    }
   });
 
   /**
@@ -233,38 +272,36 @@ module.exports = (db) => {
     *       type: integer
     *   responses:
     *     '200':
-    *       description: The Ride object which matches the rideid
+    *       description: An array which contain a record if rideID matches
     *       schema:
-    *           type: object
-    *           properties:
-    *               rideID:
-    *                   type: integer
-    *               startLat:
-    *                   type: integer
-    *               startLong:
-    *                   type: integer
-    *               endLat:
-    *                   type: integer
-    *               endLong:
-    *                   type: integer
-    *               riderName:
-    *                   type: string
-    *               driverName:
-    *                   type: string
-    *               driverVehicle:
-    *                   type: string
-    *               created:
-    *                   type: string
+    *           type: array
+    *           items:
+    *               type: object
+    *               properties:
+    *                   rideID:
+    *                       type: integer
+    *                   startLat:
+    *                       type: integer
+    *                   startLong:
+    *                       type: integer
+    *                   endLat:
+    *                       type: integer
+    *                   endLong:
+    *                       type: integer
+    *                   riderName:
+    *                       type: string
+    *                   driverName:
+    *                       type: string
+    *                   driverVehicle:
+    *                       type: string
+    *                   created:
+    *                       type: string
     *
     */
-  app.get('/rides/:id', (req, res) => {
-    db.all(`SELECT * FROM Rides WHERE rideID='${req.params.id}'`, function (err, rows) {
-      if (err) {
-        return res.send({
-          error_code: 'SERVER_ERROR',
-          message: 'Unknown error',
-        });
-      }
+  app.get('/rides/:id', async (req, res) => {
+    let rows = [];
+    try {
+      rows = await selectAllAsync('SELECT * FROM Rides WHERE rideID=?', [req.params.id]);
 
       if (rows.length === 0) {
         return res.send({
@@ -274,35 +311,15 @@ module.exports = (db) => {
       }
 
       res.send(rows);
-    });
+    } catch (err) {
+      if (err) {
+        return res.send({
+          error_code: 'SERVER_ERROR',
+          message: 'Unknown error',
+        });
+      }
+    }
   });
 
   return app;
 };
-
-
-function paginatedResults(model, page, limit) {
-  const results = {};
-  page = Number(page);
-  limit = Number(limit);
-
-  const startIndex = (page - 1) * limit;
-  const endIndex = page * limit;
-
-  if (endIndex < model.length) {
-    results.next = {
-      page: page + 1,
-      limit,
-    };
-  }
-
-  if (startIndex > 0) {
-    results.previous = {
-      page: page - 1,
-      limit,
-    };
-  }
-
-  results.results = model.slice(startIndex, endIndex);
-  return results;
-}
